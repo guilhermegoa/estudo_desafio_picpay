42cd652be75b939cb3bdc2bd77afb6b84abd74d4
“Guilherme <guilhergoa@hotmail.com>
2024-01-22 21:08:07 -0300

    feat: add sql lite datas

---
+++ Challenge.Infrastructure/Data/Migrations/20240122232908_Initial.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Challenge.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "user",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    name = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    document = table.Column<string>(type: "TEXT", maxLength: 20, nullable: false),
+                    email = table.Column<string>(type: "TEXT", maxLength: 255, nullable: false),
+                    password = table.Column<string>(type: "TEXT", nullable: false),
+                    balance = table.Column<double>(type: "REAL", nullable: false),
+                    user_type = table.Column<int>(type: "INTEGER", nullable: false),
+                    created_at = table.Column<DateTime>(type: "TEXT", nullable: false, defaultValueSql: "DATE('now')"),
+                    updated_at = table.Column<DateTime>(type: "TEXT", nullable: false, defaultValueSql: "DATE('now')")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "transaction",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    amount = table.Column<double>(type: "REAL", nullable: false),
+                    SenderId = table.Column<int>(type: "INTEGER", nullable: false),
+                    ReceiverId = table.Column<int>(type: "INTEGER", nullable: false),
+                    created_at = table.Column<DateTime>(type: "TEXT", nullable: false, defaultValueSql: "DATE('now')"),
+                    updated_at = table.Column<DateTime>(type: "TEXT", nullable: false, defaultValueSql: "DATE('now')")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_transaction", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_transaction_user_ReceiverId",
+                        column: x => x.ReceiverId,
+                        principalTable: "user",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_transaction_user_SenderId",
+                        column: x => x.SenderId,
+                        principalTable: "user",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "user",
+                columns: new[] { "id", "balance", "created_at", "document", "email", "name", "password", "user_type" },
+                values: new object[,]
+                {
+                    { 1, 1000.0, new DateTime(2024, 1, 22, 23, 29, 8, 192, DateTimeKind.Utc).AddTicks(1878), "123456789", "usuario1@example.com", "Usuário 1", "senha1", 0 },
+                    { 2, 500.0, new DateTime(2024, 1, 22, 23, 29, 8, 192, DateTimeKind.Utc).AddTicks(1887), "987654321", "usuario2@example.com", "Usuário 2", "senha2", 1 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_transaction_ReceiverId",
+                table: "transaction",
+                column: "ReceiverId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_transaction_SenderId",
+                table: "transaction",
+                column: "SenderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user_document",
+                table: "user",
+                column: "document",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user_email",
+                table: "user",
+                column: "email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "transaction");
+
+            migrationBuilder.DropTable(
+                name: "user");
+        }
+    }
+}
---
+++ Challenge.Infrastructure/Data/Migrations/20240122233149_second.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Challenge.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class second : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_transaction_user_ReceiverId",
+                table: "transaction");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_transaction_user_SenderId",
+                table: "transaction");
+
+            migrationBuilder.RenameColumn(
+                name: "SenderId",
+                table: "transaction",
+                newName: "sender_id");
+
+            migrationBuilder.RenameColumn(
+                name: "ReceiverId",
+                table: "transaction",
+                newName: "receiver_id");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_transaction_SenderId",
+                table: "transaction",
+                newName: "IX_transaction_sender_id");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_transaction_ReceiverId",
+                table: "transaction",
+                newName: "IX_transaction_receiver_id");
+
+            migrationBuilder.UpdateData(
+                table: "user",
+                keyColumn: "id",
+                keyValue: 1,
+                column: "created_at",
+                value: new DateTime(2024, 1, 22, 23, 31, 49, 469, DateTimeKind.Utc).AddTicks(9658));
+
+            migrationBuilder.UpdateData(
+                table: "user",
+                keyColumn: "id",
+                keyValue: 2,
+                column: "created_at",
+                value: new DateTime(2024, 1, 22, 23, 31, 49, 469, DateTimeKind.Utc).AddTicks(9665));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_transaction_user_receiver_id",
+                table: "transaction",
+                column: "receiver_id",
+                principalTable: "user",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_transaction_user_sender_id",
+                table: "transaction",
+                column: "sender_id",
+                principalTable: "user",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_transaction_user_receiver_id",
+                table: "transaction");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_transaction_user_sender_id",
+                table: "transaction");
+
+            migrationBuilder.RenameColumn(
+                name: "sender_id",
+                table: "transaction",
+                newName: "SenderId");
+
+            migrationBuilder.RenameColumn(
+                name: "receiver_id",
+                table: "transaction",
+                newName: "ReceiverId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_transaction_sender_id",
+                table: "transaction",
+                newName: "IX_transaction_SenderId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_transaction_receiver_id",
+                table: "transaction",
+                newName: "IX_transaction_ReceiverId");
+
+            migrationBuilder.UpdateData(
+                table: "user",
+                keyColumn: "id",
+                keyValue: 1,
+                column: "created_at",
+                value: new DateTime(2024, 1, 22, 23, 29, 8, 192, DateTimeKind.Utc).AddTicks(1878));
+
+            migrationBuilder.UpdateData(
+                table: "user",
+                keyColumn: "id",
+                keyValue: 2,
+                column: "created_at",
+                value: new DateTime(2024, 1, 22, 23, 29, 8, 192, DateTimeKind.Utc).AddTicks(1887));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_transaction_user_ReceiverId",
+                table: "transaction",
+                column: "ReceiverId",
+                principalTable: "user",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_transaction_user_SenderId",
+                table: "transaction",
+                column: "SenderId",
+                principalTable: "user",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
